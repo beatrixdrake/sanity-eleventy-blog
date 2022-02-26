1a9144337b2dbf0b1abc296848836914d7348968
beatrixdrake <bdrake@tvrbokvlt.games>
2022-02-26 00:17:21 -0600

    Forgot to import listing.js oops. It's fixed.

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -9,6 +9,7 @@ import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
+import listing from './documents/listing'
 
 // Object types
 import bodyPortableText from './objects/bodyPortableText'
